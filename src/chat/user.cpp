3305e45ab15ba7a4b2a999d88fedd0f29c3045a1
lbm <3095088766@qq.com>
2025-07-15 15:30:01 +0800

    modify user class

--- src/chat/user.cpp
+++ src/chat/user.cpp
@@ -3,16 +3,20 @@
 json User::toJson() const
 {
     return json{
-        {"username", username},
-        {"password", password},
-        {"is_online", is_online}};
+        {"id", id_},
+        {"username", username_},
+        {"password", password_},
+        {"is_online", is_online_},
+        {"last_active_time", last_active_time_}};
 }
 
 User User::fromJson(const json &j)
 {
     User user;
-    user.username = j.at("username").get<std::string>();
-    user.password = j.at("password").get<std::string>();
-    user.is_online = j.at("is_online").get<bool>();
+    user.id_ = j.value("id", "");
+    user.username_ = j.at("username").get<std::string>();
+    user.password_ = j.at("password").get<std::string>();
+    user.is_online_ = j.at("is_online").get<bool>();
+    user.last_active_time_ = j.value("last_active_time", 0);
     return user;
 }
--- src/chat/user.hpp
+++ src/chat/user.hpp
@@ -4,12 +4,38 @@
 
 using json = nlohmann::json;
 
-struct User
+class User
 {
-    std::string username;
-    std::string password;
-    bool is_online;
+private:
+    std::string id_;//用户id
+    std::string username_;//用户姓名
+    std::string password_;//用户密码
+    bool is_online_;//用户在线状态
+    int64_t last_active_time_;//用户最后活跃时间
 
+public:
+    // 构造函数
+    User() : is_online_(false), last_active_time_(0) {}//默认构造函数
+    User(const std::string &id, const std::string &username, const std::string &password,
+         bool is_online = false, int64_t last_active_time = 0)
+        : id_(id), username_(username), password_(password),
+          is_online_(is_online), last_active_time_(last_active_time) {}
+
+    // Getter方法
+    const std::string &getId() const { return id_; }
+    const std::string &getUsername() const { return username_; }
+    const std::string &getPassword() const { return password_; }
+    bool isOnline() const { return is_online_; }
+    int64_t getLastActiveTime() const { return last_active_time_; }
+
+    // Setter方法
+    void setId(const std::string &id) { id_ = id; }
+    void setUsername(const std::string &username) { username_ = username; }
+    void setPassword(const std::string &password) { password_ = password; }
+    void setOnline(bool is_online) { is_online_ = is_online; }
+    void setLastActiveTime(int64_t last_active_time) { last_active_time_ = last_active_time; }
+
+    // JSON转换
     json toJson() const;
-    static User fromJson(const json &j); // 传入JSON数据并返回一个JSON对象，是一个工厂方法
+    static User fromJson(const json &j);
 };
